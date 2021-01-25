55a48ca77e11eee2dbc190f4b82a0ebc5a8156a6
Patrick Schwan <info@pschwan.de>
2021-01-25 19:22:00 +0100

    added HelloAPIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """Test API View"""
+
+    # self wegen class, request kommt vom rest_framework und BestPractise
+    # dass das format=None gesetzt ist, falls mal format für die Endpoints
+    # aktiviert wird, ist es bereits unterstützt
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        # is an example for demo
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your app logic'
+            'is mapped manually to URLs'
+        ]
+
+        # return a Response Object is expected
+        # due to JSON output it has to be a list or dict - here a dict
+        return Response({'message':'Hello!', 'an_apiview': an_apiview})
